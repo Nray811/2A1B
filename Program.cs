d9e6e7147f3c7c5a08888552a7f95ce31a0f1404
Nray <ray08118686@gmail.com>
2023-02-14 15:18:46 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2A1B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+            string[] Data = new string[4];
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Data[i] = r.Next(1, 10).ToString();
+                for (int j = 0; j < i; j++)
+                {
+                    if (Data[i] == Data[j])
+                    {
+                        j = 0;
+                        Data[i] = r.Next(1, 10).ToString();
+                    }
+                }
+              
+            }
+            string gm = Data[0] + Data[1] + Data[2] + Data[3];
+            Console.Write("猜的4個號碼為： ");
+            string guess=""; 
+            guess = Console.ReadLine();
+            int a = 0, b = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (guess[i] == gm[j])
+                    {
+                        if (i == j)
+                        {
+                            a++;
+                        }                           
+                        else b++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"電腦選號為:{gm}");
+            Console.WriteLine($"結果是{a}A{b}B");
+
+        }
+
+        }
+        }
+
+    
+
+
